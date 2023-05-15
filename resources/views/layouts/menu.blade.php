c46cbef4cc17be37f3c5d161ddb29fadf6e0d231
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-05-15 13:27:07 +0100

    feat: Ajout du menu bssid pour ajouter la liste des bssid autorisés

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -5,3 +5,9 @@
         <p>Home</p>
     </a>
 </li>
+<li class="nav-item">
+    <a href="{{ route('bssid.index') }}"  class="nav-link {{ Request::is('bssid') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-wifi"></i>
+        <p>Bssid</p>
+    </a>
+</li>
