9078cc53dc6c7e669c6324a74fd533a7b53f0f4b
K-ris <christkuzanuka@gmail.com>
2023-05-16 16:45:32 +0100

    Intégration de l'api absence

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+require 'api_absence.php';
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
