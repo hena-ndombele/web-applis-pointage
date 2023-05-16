84ecb7053e16fbce5494f37876c5eccfb2e7f849
K-ris <christkuzanuka@gmail.com>
2023-05-16 16:40:09 +0100

    Définition de la table fille

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function absences(){
+        return $this->hasMany(Absence::class);
+    }
 }
