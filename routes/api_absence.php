bf23398c13764a428e4f090d2a7a6f2fdb2cb5e8
K-ris <christkuzanuka@gmail.com>
2023-05-19 12:04:04 +0100

    Intégration de l'api absence

--- routes/api_absence.php
+++ routes/api_absence.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AbsenceController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::post('/absences', [AbsenceController::class, 'store']);
 Route::get('/absences/cancel/{id}', [AbsenceController::class, 'cancel']);
 Route::get('/absences', [AbsenceController::class, 'show']);
