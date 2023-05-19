503501d461b611f260c5627c7cef9129630cfcce
K-ris <christkuzanuka@gmail.com>
2023-05-19 12:02:42 +0100

    Migration de l'ajout du champ status à la table absence

--- database/migrations/2023_05_16_134757_add_column_to_absences.php
+++ database/migrations/2023_05_16_134757_add_column_to_absences.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::table('absences', function (Blueprint $table) {
             $table->enum('status', ['active', 'offline'])->default('active');
         });
+        
     }
 
     /**
