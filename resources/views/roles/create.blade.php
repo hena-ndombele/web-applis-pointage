0d14fbbc9b56c15a9d9c3739b66c8bee88d34053
DanAlex08 <105365781+DanAlex08@users.noreply.github.com>
2023-05-31 16:19:47 +0100

    Gestion des utilisiateurs et des autorisations sur chaque entite

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -1,52 +1,53 @@
 @extends('layouts.app')
-  
+@section('title')
+    <span>Ajouter d'un nouveau role</span>
+@endsection
 @section('content')
-<section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Ajouter un nouveau role</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">DataTables</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
 
-   
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Oups! </strong> Il y a eu des problèmes avec votre entrée.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-   <div class=" col-xs-6 col-sm-6 col-md-6">
-    <form action="{{ route('roles.store') }}" method="POST">
-        @csrf
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                <div class="form-group">
-                    
-                    <input type="text" name="name" class="form-control" placeholder="Saisir un libellé">
-                </div>
-            </div>
-            <div class="row">
-            <div class="col-xs-6 col-sm-12 col-md-6 text-left">
-                    <button type="submit" class="btn btn-primary">Soumettre</button>
-            </div>
+<div class="container">
+    <div class="col-md-12">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Oups! </strong> Il y a eu des problèmes avec votre entrée.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-        
-        
-        
-       
-    </form>
-   </div>
+        @endif
+        <div class="card">
+            <div class="card-body">
+              <div class=" col-xs-12 col-sm-12 col-md-12">
+                <form action="{{ route('roles.store') }}" method="POST">
+                    @csrf
+                      <div class="container">
+                          <div class="row">
+                              <div class="">
+                                  <div class="form-group">
+                                      <input type="text" name="name" class="form-control" placeholder="Saisir un libellé">
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="row">
+                              <div class="col-xs-6 col-sm-12 col-md-12 text-right">
+                                  <button type="submit" class="btn btn-light"><i class="fas fa-save"></i></button>
+                          </div>
+                          </div>
+                      </div>
+          
+          
+                </form>
+               </div>
+            </div>
+          </div>
+    </div>
+
+</div>
 
 @endsection
+
+
+
+
+
--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -1,70 +1,61 @@
 @extends('layouts.app')
-@section('content')
-<section class="content-header">
-   <div class="container-fluid">
-     <div class="row mb-2">
-       <div class="col-sm-6">
-         <h1>{{$role->name}}</h1>
-       </div>
-       <div class="col-sm-6">
-         <ol class="breadcrumb float-sm-right">
-           <li class="breadcrumb-item"><a href="#">Home</a></li>
-           <li class="breadcrumb-item active">DataTables</li>
-         </ol>
-       </div>
-     </div>
-   </div><!-- /.container-fluid -->
- </section>
-<div class="card">
-    <div class="card-body">
-        
-          <form action="{{route('roles.modify', $role->id)}}" method="POST">
-            @csrf
-            <input type="hidden" name="role_id" value="{{$role->id}}" id="">
-            <table class="table table-bordered">
-               <thead>
-                  <tr>
-                     <th style="width: 10px">#</th>
-                     <th>Entité</th>
-                     <th>Create</th>
-                     <th>Read</th>
-                     <th>Update</th>
-                     <th>Delete</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  @foreach ($models as $model)
-                  <tr>
-                     <td> {{++$i}} </td>
-                     <td>
-                        {{ $model }}
-                     </td>
-                     @foreach ($actions as $action)
-                     <td class="icheck-primary  ">
-                           <input type="checkbox" id="" 
-                           @php
-                           $verify =  DB::select('select * from role_police where role_id = ? and model = ? and action = ? ', [$role->id, $model, $action] );
-                           @endphp
-                           @if ($verify)
-                           checked
-                           @else
-                                                    
-                           @endif
-                             name="form[{{$model.'_'.$action}}]"  class="justify-center" spellcheck="false">
-                     </td>
-                     @endforeach
-     
-                  </tr>
-                  @endforeach
-               </tbody>
-            </table><br>
-            <div class="float-end">
-             <button type="submit" class="btn btn-outline-primary">Editer</button>
-          </div>
-
-          </form>
-          
-   
-    </div>
- </div>
+@section('title')
+   <span>Role {{$role->name}}</span>
 @endsection
+@section('content')
+      <div class="container">
+         <div class="card">
+            <div class="card-body">
+                  <form action="{{route('roles.modify', $role->id)}}" method="POST">
+                    @csrf
+                    <input type="hidden" name="role_id" value="{{$role->id}}" id="">
+                    <table class="table table-striped">
+                       <thead>
+                          <tr>
+                             <th class="col-1">#</th>
+                             <th class="col-5 text-left">Entité</th>
+                             <th>Créer</th>
+                             <th>Lire</th>
+                             <th>Modifier</th>
+                             <th>Supprimer</th>
+                          </tr>
+                       </thead>
+                       <tbody>
+                          @foreach ($models as $model)
+                          <tr>
+                             <td class="col-1 text-center"> {{++$i}} </td>
+                             <td class="col-5 text-left">
+                                {{ $model }}
+                             </td>
+                             @foreach ($actions as $action)
+                             <td class="icheck-primary col-2  ">
+                                   <input type="checkbox" id=""
+                                   @php
+                                   $verify =  DB::select('select * from role_police where role_id = ? and model = ? and action = ? ', [$role->id, $model, $action] );
+                                   @endphp
+                                   @if ($verify)
+                                   checked
+                                   @else
+                                   @endif
+                                     name="form[{{$model.'_'.$action}}]"  class="justify-center" spellcheck="false">
+                             </td>
+                             @endforeach
+                          </tr>
+                          @endforeach
+                       </tbody>
+                    </table>
+                    <div>
+                     <button type="submit" class="btn btn-default border float-right"><i class="fas fa-save"></i></button>
+                  </div>
+                  </form>
+            </div>
+         </div>
+      </div>
+      
+@endsection
+
+
+
+
+
+
