3ccab1a677a9e8fbde94a9403c66599db4f43713
hena-ndombele <henandombele2021@gmail.com>
2023-06-16 17:25:45 +0100

    ajout des routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\AbsenceController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -9,8 +8,6 @@ use App\Http\Controllers\CongeController;
 use App\Http\Controllers\PresenceController;
 use App\Http\Controllers\DemandeCongeController;
 @include('webAcl.php');
-use App\Http\Controllers\TauxConfigurationController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,34 +18,20 @@ use App\Http\Controllers\TauxConfigurationController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return redirect('/home');
 });
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-require('web_bssid.php'); 
+require('web_bssid.php');
 Route::resource('presences', PresenceController::class);
-
-
-
-
-Route::resource('bssid', BssidController::class); 
-
-Route::resource('bssid', BssidController::class);
-
-
-
-Route::resource('presences', PresenceController::class)->middleware('checkaccess:Presence');
-
-
-Route::resource('bssid', BssidController::class); 
-
+Route::resource('conge',CongeController::class);
+Route::resource('demandes', DemandeCongeController::class);
 Auth::routes();
 Route::resource('absences', AbsenceController::class);
-Route::resource('taux', TauxConfigurationController::class);
-
-
-
 Route::get('print/{bssid}', [BssidController::class, 'print'])->name('print');
+Route::get('/show-presences/{date}', [PresenceController::class, 'show'])->name('show_presences');
 require('web_taux.php');
+
+
+
+
