5601fae3d967d65885f7709bae313d67b0ade83f
berdin matondo <berdinmatondo7@gmail.com>
2023-06-23 15:13:09 +0100

    fix: ajouts des routes fonctions

--- routes/web_horaires.php
+++ routes/web_horaires.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContratController;
 use App\Http\Controllers\HoraireController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FonctionController;
 
 Route::resource('horaires', HoraireController::class);
 Route::resource('contrats', ContratController::class);
+Route::resource('fonctions', FonctionController::class);
