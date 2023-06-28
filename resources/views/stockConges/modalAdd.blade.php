38609b37c4af45b7d53d5648575f7d5bdc00aea8
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-06-28 10:01:38 +0100

    Feat: Mise en place des stock congés annuels

---
+++ resources/views/stockConges/modalAdd.blade.php
@@ -0,0 +1,38 @@
+
+
+<div class="modal fade" id="modalAddForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                    <h4 class="modal-title w-100 font-weight-bold">Stock congé Annuel</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
+            <form action="{{ route('stockConges.store') }}" method="post">
+                @csrf
+                <div class="modal-body mx-3">
+                    <div class="md-form mb-2">
+                        <i class="fa fa-font"></i>
+                        <label>Grade Agent</label>
+                        <select name="grade" class="form-control">
+                            @foreach ($grades as $grade)
+                                <option value="{{ $grade }}">{{ $grade }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="md-form mb-3">
+                        <i class="fa fa-table"></i>
+                        <label>Nombre des jours du congé</label>
+                        <input type="text" name="totalConge" class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer d-flex justify-content-center">
+                    <input type="submit" class="btn btn-primary" value="Enregistrer">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
---
+++ resources/views/stockConges/stockCongeList.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+
+@section('title')
+    <span>Liste des congés prevus par la lois</span>
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+
+
+         
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>  
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+        @include('stockConges.modalDelete')
+        @include('stockConges.modalAdd')
+        
+
+  
+       
+
+        <div class="d-flex flex-row-reverse bd-highlight">
+            <div class="p-2 bd-highlight">
+                <input type="submit" class="btn btn-primary btn-rounded mb-4" data-toggle="modal" data-target="#modalAddForm" value="+Ajouter">
+            </div>
+        </div>
+
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">N°</th>
+                <th scope="col">Grade agent</th>
+                <th scope="col">Nombre des jours</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+                @php
+                    $number = 1;
+                @endphp
+                
+                @forelse ($stocks as $item) 
+                    <tr>
+                        <th scope="row">{{ $number }}</th>
+                        <td>{{ $item->grade }}</td>
+                        <td>{{ $item->totalConge }}</td>
+                        <td>
+                            <div class="row">
+                                <a class="btn btn-danger mr-2" onclick="supprimer(event)" href="{{ route('stockConges.destroy', $item->id)}}" data-toggle="modal" data-target="#modalDeleteForm" ><i class="fas fa-trash"></i></a>
+                            </div>
+                        </td>
+                    </tr>
+                @php
+                    $number++;
+                @endphp
+                @empty
+                    <tr>
+                        <td colspan="5"> 
+                            Aucun enregistrement
+                        </td>
+                  </tr>
+                @endforelse
+            </tbody>
+          </table> 
+    </div>
+
+
+    <script>
+        function supprimer(event) {
+            var route = event.target.getAttribute('href')
+            deleteForm.setAttribute('action', route)
+            //alert(route)
+        }
+    </script>
+@endsection
