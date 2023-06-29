17c9d70e9d0d3c62a2d12039e394da1876751b04
Your Name <you@example.com>
2023-06-29 11:54:37 +0100

    mise en liaison du serveur node et laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,41 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use App\Models\DemandeConge;
 
 class PostController extends Controller
 {
-    public function envoyerDonneesAuServeur()
-    {
-        
-        $client = new Client();
-        $reponse = $client->post('http://localhost:5000/reception', [
-            'form_params' => [
-                'nom' => 'John Doe',
-                'email' => 'john.doe@example.com',
-                'message' => 'Bonjour, comment allez-vous ?',
-            ]
-        ]);
 
-        dd($reponse->getBody());
+               
+
+    static function index($id)
+    {
+        $demande = DemandeConge::findOrFail($id);
+        if($demande->id!=""){
+            try{
+                
+                
+                $client = new Client();
+                $reponse = $client->post('http://localhost:5000/reception', [
+                    'form_params' => [
+                        'id'=>$demande->id,
+                        'user_id' => $demande->user_id,
+                        'debut' => $demande->debut,
+                        'fin' =>$demande->Fin,
+                        'status'=>$demande->status,
+                    ]
+                ]);
+        
+                $reponse->getBody();
+                
+          
+            }
+            catch(\Exception $e){
+            }
+        }
+       
+       
+      
+       
     }
 }
